786c6feca1a75e2ed4d0e6df32d7148483a43f8c
tkasuz <63289889+tkasuz@users.noreply.github.com>
2026-03-08 12:32:55 +0900

    full template

--- src/tfcmt.ts
+++ src/tfcmt.ts
@@ -9,59 +9,141 @@ import * as core from '@actions/core';
 import * as tc from '@actions/tool-cache';
 
 /**
- * The custom tfcmt plan template.
+ * Full tfcmt configuration based on the upstream default, with the plan
+ * template extended to show a "Run this plan again" command using -project=.
  *
- * Renders the plan output and appends a "Run this plan again" hint using the
- * project name from the `target` tfcmt variable (passed via `-var "target:<name>"`).
+ * @see https://suzuki-shunsuke.github.io/tfcmt/config/#default-configuration
  */
-const PLAN_TEMPLATE = `
-{{- $target := index .Vars "target" -}}
-{{- if eq .ExitCode 1 -}}
-## ❌ Terraform Plan Failed{{ if $target }} (\`{{ $target }}\`){{ end }}
-{{- else if eq .ExitCode 0 -}}
-## ✅ Plan: No Changes{{ if $target }} (\`{{ $target }}\`){{ end }}
-{{- else -}}
-## 📋 Terraform Plan{{ if $target }} (\`{{ $target }}\`){{ end }}
-{{- end }}
+const TFCMT_CONFIG = `
+embedded_var_names: []
 
-{{ if .Result -}}
-<details><summary>Show Plan</summary>
+templates:
+  plan_title: "## {{if eq .ExitCode 1}}:x: {{end}}Plan Result{{if .Vars.target}} ({{.Vars.target}}){{end}}"
+  apply_title: "## :{{if eq .ExitCode 0}}white_check_mark{{else}}x{{end}}: Apply Result{{if .Vars.target}} ({{.Vars.target}}){{end}}"
+  result: "{{if .Result}}<pre><code>{{ .Result }}</code></pre>{{end}}"
+  updated_resources: |
+    {{if .CreatedResources}}
+    * Create
+    {{- range .CreatedResources}}
+      * {{.}}
+    {{- end}}{{end}}{{if .UpdatedResources}}
+    * Update
+    {{- range .UpdatedResources}}
+      * {{.}}
+    {{- end}}{{end}}{{if .DeletedResources}}
+    * Delete
+    {{- range .DeletedResources}}
+      * {{.}}
+    {{- end}}{{end}}{{if .ReplacedResources}}
+    * Replace
+    {{- range .ReplacedResources}}
+      * {{.}}
+    {{- end}}{{end}}{{if .ImportedResources}}
+    * Import
+    {{- range .ImportedResources}}
+      * {{.}}
+    {{- end}}{{end}}{{if .MovedResources}}
+    * Move
+    {{- range .MovedResources}}
+      * {{.Before}} => {{.After}}
+    {{- end}}{{end}}
+  deletion_warning: |
+    {{if .HasDestroy}}
+    ### :warning: Resource Deletion will happen :warning:
+    This plan contains resource delete operation. Please check the plan result very carefully!
+    {{end}}
+  changed_result: |
+    {{if .ChangedResult}}
+    <details><summary>Change Result (Click me)</summary>
+    {{wrapCode .ChangedResult}}
+    </details>
+    {{end}}
+  change_outside_terraform: |
+    {{if .ChangeOutsideTerraform}}
+    <details><summary>:information_source: Objects have changed outside of Terraform</summary>
+    _This feature was introduced from [Terraform v0.15.4](https://github.com/hashicorp/terraform/releases/tag/v0.15.4)._
+    {{wrapCode .ChangeOutsideTerraform}}
+    </details>
+    {{end}}
+  warning: |
+    {{if .Warning}}
+    ## :warning: Warnings :warning:
+    {{wrapCode .Warning}}
+    {{end}}
+  error_messages: |
+    {{if .ErrorMessages}}
+    ## :warning: Errors
+    {{range .ErrorMessages}}
+    * {{. -}}
+    {{- end}}{{end}}
+  guide_apply_failure: ""
+  guide_apply_parse_error: ""
 
-{{ .Result }}
-
-</details>
-{{- end }}
-{{ if .ChangeOutsideTerraform -}}
-<details><summary>⚠️ Changes Outside Terraform</summary>
-
-{{ .ChangeOutsideTerraform }}
-
-</details>
-{{- end }}
-{{ if .Warning }}
-> ⚠️ {{ .Warning }}
-{{ end }}
----
-**Run this plan again:**
-\`\`\`
-terraform plan{{ if $target }} -project={{ $target }}{{ end }}
-\`\`\`
-{{ if .Link }}[CI Details]({{ .Link }}){{ end }}
+terraform:
+  plan:
+    disable_label: false
+    ignore_warning: false
+    template: |
+      {{template "plan_title" .}}
+      {{if .Link}}[CI link]({{.Link}}){{end}}
+      {{template "deletion_warning" .}}
+      {{template "result" .}}
+      {{template "updated_resources" .}}
+      {{template "changed_result" .}}
+      {{template "change_outside_terraform" .}}
+      {{template "warning" .}}
+      {{template "error_messages" .}}
+      {{if .Vars.target}}
+      ---
+      **Run this plan again:** \`terraform plan -project={{.Vars.target}}\`
+      {{end}}
+    when_add_or_update_only:
+      label: "{{if .Vars.target}}{{.Vars.target}}/{{end}}add-or-update"
+      label_color: 1d76db
+    when_destroy:
+      label: "{{if .Vars.target}}{{.Vars.target}}/{{end}}destroy"
+      label_color: d93f0b
+    when_no_changes:
+      label: "{{if .Vars.target}}{{.Vars.target}}/{{end}}no-changes"
+      label_color: 0e8a16
+    when_plan_error:
+      label:
+      label_color:
+    when_parse_error:
+      template: |
+        {{template "plan_title" .}}
+        {{if .Link}}[CI link]({{.Link}}){{end}}
+        It failed to parse the result.
+        <details><summary>Details (Click me)</summary>
+        {{wrapCode .CombinedOutput}}
+        </details>
+  apply:
+    template: |
+      {{template "apply_title" .}}
+      {{if .Link}}[CI link]({{.Link}}){{end}}
+      {{if ne .ExitCode 0}}{{template "guide_apply_failure" .}}{{end}}
+      {{template "result" .}}
+      <details><summary>Details (Click me)</summary>
+      {{wrapCode .CombinedOutput}}
+      </details>
+      {{template "error_messages" .}}
+    when_parse_error:
+      template: |
+        {{template "apply_title" .}}
+        {{if .Link}}[CI link]({{.Link}}){{end}}
+        {{template "guide_apply_parse_error" .}}
+        It failed to parse the result.
+        <details><summary>Details (Click me)</summary>
+        {{wrapCode .CombinedOutput}}
+        </details>
 `.trimStart();
 
 /**
- * Writes a tfcmt configuration file with a custom plan template to a temp
- * directory and returns the path to it.
+ * Writes a tfcmt configuration file to a temp directory and returns the path.
  */
 export function writeTfcmtConfig(): string {
-  const configContent = `terraform:
-  plan:
-    template: |
-${PLAN_TEMPLATE.split('\n').map((line) => `      ${line}`).join('\n')}
-`;
-
   const configPath = path.join(os.tmpdir(), '.tfcmt-action.yml');
-  fs.writeFileSync(configPath, configContent, 'utf8');
+  fs.writeFileSync(configPath, TFCMT_CONFIG, 'utf8');
   core.info(`tfcmt config written to ${configPath}`);
   return configPath;
 }
